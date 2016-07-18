79177d261ffe4bfc5446167e167d27b66129c9b1
ZheldakArtem <artemzheldak1995@gmail.com>
2016-07-18 23:11:12 +0300

    Add service layer

--- Task1/Task4/BinaryBookListstorage.cs
+++ Task1/Task4/BinaryBookListstorage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,23 +8,85 @@ using Task_Book;
 
 namespace Task4
 {
-    public class BinaryBookListstorage : IBookListStorage
+    public class BinaryBookListStorage : IBookListStorage
     {
-        private readonly string _fileName;
+        private readonly string _filePath;
 
-        public BinaryBookListstorage(string fileName)
+        public BinaryBookListStorage(string filePath)
         {
-            _fileName = fileName;
+            _filePath = filePath;
         }
-
+        /// <summary>
+        /// Method load books in List
+        /// </summary>
+        /// <returns>Collection of books</returns>
         public List<Book> LoadBooks()
         {
-            throw new NotImplementedException();
-        }
+            var listBooks=new List<Book>();
+            try
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(_filePath, FileMode.Open)))
+                {
+                    while (reader.PeekChar() > -1)
+                    {
+                        Book book = new Book(reader.ReadString(), reader.ReadString(), reader.ReadString(),
+                            reader.ReadInt32(), reader.ReadInt32());
 
+                        listBooks.Add(book);
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                throw;
+            }
+            catch (FileLoadException e)
+            {
+                throw;
+            }
+            catch (IOException e)
+            {
+                throw;
+            }
+            catch (ArgumentNullException e)
+            {
+                throw;
+            }
+    
+            return listBooks;
+        }
+        /// <summary>
+        /// Save the collection of books in the file
+        /// </summary>
+        /// <param name="books"></param>
         public void SaveBooks(IEnumerable<Book> books)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.Open(_filePath, FileMode.OpenOrCreate)))
+                {
+                    foreach (Book book in books)
+                    {
+                        writer.Write(book.Author);
+                        writer.Write(book.Title);
+                        writer.Write(book.Genre);
+                        writer.Write(book.Publication);
+                        writer.Write(book.Pages);
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                throw;
+            }
+            catch (IOException e)
+            {
+                throw;
+            }
+            catch (ArgumentNullException)
+            {
+                throw;
+            }
         }
     }
 }
--- Task1/Task4/BookListService.cs
+++ Task1/Task4/BookListService.cs
@@ -15,38 +15,83 @@ namespace Task4
         public BookListService(IBookListStorage bookListStorage)
         {
             _bookListStorage = bookListStorage;
+            _books = LoadBooks();
         }
-
+        /// <summary>
+        /// Invoke BinaryBookListStorage's method LoadBooks
+        /// </summary>
+        /// <returns>Collection of books</returns>
         public List<Book> LoadBooks()
         {
             return _bookListStorage.LoadBooks();
         }
-
+        /// <summary>
+        /// Invoke BinaryBookListStorage's method SaveBooks
+        /// </summary>
+        /// <param name="books">Collection of books</param>
         public void SaveBooks(IEnumerable<Book> books)
         {
-            _bookListStorage.SaveBooks(_books);
+            _bookListStorage.SaveBooks(books);
         }
-
+        /// <summary>
+        /// Add book to the collection
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
         public bool AddBook(Book book)
         {
+            if (ReferenceEquals(book, null))
+            {
+                throw new ArgumentNullException();
+            }
+            if (_books.Contains(book))
+            {
+                return false;
+            }
             _books.Add(book);
             return true;
         }
 
         public bool RemoveBook(Book book)
         {
-            _books.Remove(book);
-            return true;
-        }
+            if (ReferenceEquals(book, null))
+            {
+                throw new ArgumentNullException();
+            }
+            if (_books.Contains(book))
+            {
+                _books.Remove(book);
+                return true;
+            }
 
-        public Book FindBookByTag()
-        {
-            return null;
+            return false;
         }
-
-        public void SortBookByTag()
+        /// <summary>
+        /// Searches for an element that matches the conditions defined by the specified predicate, and returns the first occurrence within the entire.
+        /// </summary>
+        /// <param name="books">Collection of books</param>
+        /// <param name="predicate">The delegate that defines the conditions of the element to search for</param>
+        /// <returns>The first element that matches the conditions defined by the specified predicate, if found; otherwise, the default value for type</returns>
+        public Book FindBooksByTag(List<Book> books, Predicate<Book> predicate)
         {
-            
+            if (ReferenceEquals(predicate, null) || ReferenceEquals(books, null))
+            {
+                throw new ArgumentNullException();
+            }
+            return books.Find(predicate);
+        }
+        /// <summary>
+        /// Sorts the elements in the collection using the specified delegate.
+        /// </summary>
+        /// <param name="books">Collection of books</param>
+        /// <param name="comparison">The delegate to use when comparing elements.</param>
+        public void SortBooksByTag(List<Book> books, Comparison<Book> comparison)
+        {
+            if (ReferenceEquals(comparison, null) || ReferenceEquals(books, null))
+            {
+                throw new ArgumentNullException();
+            }
+            books.Sort(comparison);
         }
     }
 }
